63718ece6fe705dd7ffc8d5c58e0775bbe71ab4f
Sushant Ramesh Joshilkar <132161926+Sushant-Joshilkar04@users.noreply.github.com>
2024-11-19 00:40:27 +0530

    FIFO_page.c

--- FCFS_page.c
+++ FCFS_page.c
@@ -1,34 +1,48 @@
+#include<stdio.h>
+#include<stdbool.h>
 
-#include <stdio.h>
-
-void display(int size,int frame[]){
+void display(int frame[],int size){
     for(int i=0;i<size;i++)
-       printf("%d ",frame[i]); 
-    printf("\n");
+     printf(" %d ",frame[i]);
+     printf("\n");
 }
 
-int main() {
+bool contains(int frame[],int size,int ele){
+    for (int i = 0; i < size; i++)
+       if(frame[i]==ele)
+         return true;
+    
+    return false;
+}
+
+int main(){
     int n;
-    printf("Enter number of pages: ");
+    printf("Enter number of pages:");
     scanf("%d",&n);
-    int arr[n];
+    int pages[n];
     printf("Enter pages: ");
-    for(int i=0;i<n;i++) 
-    scanf("%d",&arr[i]);
-    int size = 0;
-    printf("Enter page frame size: ");
+    for(int i=0;i<n;i++)
+      scanf("%d",&pages[i]);
+
+    int size;
+    printf("Enter pages frame size: ");
     scanf("%d",&size);
-    int frame[size] ;
-     for(int i=0;i<size ;i++) 
-        frame[i] = -1;
-     display(size,frame);
-     int j=0;
-     for(int i=0;i< n;i++) {
-        frame[j] = arr[i];
-        j++;
-        j %= size;
-        display(size,frame);
+    int frame[size];
+
+    for(int i=0;i<size;i++) frame[i] = -1;
+
+    int j=0,hit=0,fault=0; 
+    for(int i=0;i<n;i++){
+        if(contains(frame,size,pages[i])){
+           hit++;
+           display(frame,size);
+        }else{
+            frame[j%size] = pages[i];
+            fault++;j++;
+            display(frame,size);
+        }
     }
 
+    printf("Hit: %d Fault: %d Pages: %d",hit,fault,n);
     return 0;
 }
