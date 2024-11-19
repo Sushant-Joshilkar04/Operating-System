b77b0e9702a949f259f801a6b8f2fe2f3294ea90
Sushant Ramesh Joshilkar <132161926+Sushant-Joshilkar04@users.noreply.github.com>
2024-11-19 14:51:45 +0530

    FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
-#include <stdbool.h>
-#include<limits.h>
+#include<stdbool.h>
 
 void display(int n,int pid[],int at[],int bt[],int st[],int et[],int wt[],int tat[]){
     printf("PID   Arrival  Burst  Start    End   waiting  Turn_Around\n");
@@ -15,33 +14,33 @@ void Swap(int *a ,int *b){
     *b = temp;
 }
 
-void answer(int n,int pid[],int at[],int bt[]){
-    int st[n];
-    int et[n];
-    int wt[n];
-    int tat[n];
+void sort_according_to_at(int pid[],int at[],int bt[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+          if(at[j]>at[j+1]){
+              Swap(&at[j],&at[j+1]);
+                Swap(&bt[j],&bt[j+1]);
+                Swap(&pid[j],&pid[j+1]);
+          }
+       }   
+ }
+}
 
-    for(int i=0;i<n;i++){
-        st[i] = 0; 
-        et[i] = 0;
-        wt[i] = 0;
-        tat[i] = 0;
-    }
+void answer(int n, int pid[], int at[],int bt[],int st[],int et[],int wt[],int tat[]){
 
     st[0] = at[0];
     et[0] = at[0]+bt[0];
+     for (int i = 1; i < n; i++)
+     {  if((et[i-1]>=at[i])){
+        st[i] = et[i-1];
+        et[i] = st[i]+bt[i];
+        }else{
+        st[i] = at[i];
+        et[i] = at[i]+bt[i]; 
+        }
+     }
 
-   for(int i=1;i<n;i++){
-    if(et[i-1]>=at[i]){
-    st[i] = et[i-1];
-    et[i] = st[i]+bt[i];
-    }else{
-    st[i] = at[i];
-    et[i] = at[i]+bt[i];
-    }
-   }
-
-    float avgWt=0,avgTat=0;
+     float avgWt=0,avgTat=0;
     for (int i = 0; i < n; i++) {
         tat[i] = et[i] - at[i];
         wt[i] = tat[i] - bt[i];
@@ -49,44 +48,35 @@ void answer(int n,int pid[],int at[],int bt[]){
         avgTat=avgTat+tat[i];
     }
 
-    display(n, pid, at, bt, st, et, wt, tat);
-    printf("\nAverage WaitingTime -> %.2f",avgWt/n);
-    printf("\nAverage TurnAroundTime -> %.2f",avgTat/n);
-}
-
-void sort_according_to_at(int n,int pid[],int at[],int bt[]){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(at[j]>at[j+1]){
-                Swap(&at[j],&at[j+1]);
-                Swap(&bt[j],&bt[j+1]);
-                Swap(&pid[j],&pid[j+1]);
-            }
-        }
-    }
-
-    answer(n, pid, at, bt);
+    display(n,pid, at, bt, st, et, wt,tat);
+    printf("\nAverage WaitingTime -> %.2f\n",avgWt/n);
+    printf("\nAverage TurnAroundTime -> %.2f\n",avgTat/n);
 }
 
 int main(){
-int n;
-printf("Enter total number of processors: ");
-scanf("%d",&n);
+    printf("Enter number of processors: ");
+    int n;
+    scanf("%d",&n);
+    int pid[n],at[n],bt[n],st[n],et[n],wt[n],tat[n];
 
-int pid[n];
-int at[n];
-int bt[n];
+    printf("Enter pid,Arrival Time,Burst Time\n");
+    for (int i = 0; i < n; i++)
+    scanf("%d %d %d",&pid[i],&at[i],&bt[i]);   
+    
+    for(int i=0;i<n;i++){
+        st[i] = 0; 
+        et[i] = 0;
+        wt[i] = 0;
+        tat[i] = 0;
+    }
 
+    display(n,pid, at, bt, st, et, wt,tat);
+    
+    sort_according_to_at( pid, at,bt, n);
 
-printf("Enter Processor id , arrival time ,burst time :\n ");
-for(int i=0;i<n;i++){
-     scanf("%d",&pid[i]);
-     scanf("%d",&at[i]);
-     scanf("%d",&bt[i]);
+    answer(n,pid, at, bt, st, et, wt,tat);
+
+     display(n,pid, at, bt, st, et, wt,tat);
+
+    return 0;
 }
-
-sort_according_to_at(n, pid, at, bt);
-
-return 0;
-}
-
