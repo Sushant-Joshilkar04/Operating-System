e362d37a5516349b9c5136f243092fae0bb5e666
Sushant Ramesh Joshilkar <132161926+Sushant-Joshilkar04@users.noreply.github.com>
2024-11-21 22:45:21 +0530

    . fork.c

--- fork.c
+++ fork.c
@@ -1,94 +1,83 @@
 #include<stdio.h>
-#include<unistd.h>
 #include<stdbool.h>
+#include<unistd.h>
 #include<stdlib.h>
 
-void display(int arr[],int n){
-  for(int i=0;i<n;i++)
-  {
-    printf("%d ",arr[i]);
+
+
+void bubbleSortAscending(int arr[],int n){
+  bool swapped;
+  for(int i=0;i<n-1;i++){
+    swapped = false;
+    for(int j=0;j<n-i-1;j++){
+      if(arr[j]>arr[j+1]){
+        int temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        swapped = true;
+      }
+    }
+    if(swapped == false) break;
   }
-  printf("\n");
 }
 
-void swap(int* xp, int* yp){
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
 
-void bubbleSortAscending(int arr[], int n){
-    int i, j;
-    bool swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(&arr[j], &arr[j + 1]);
-                swapped = true;
-            }
-        }
-
-        if (swapped == false)
-            break;
+void bubbleSortDescending(int arr[],int n){
+  bool swapped;
+  for(int i=0;i<n-1;i++){
+    swapped = false;
+    for(int j=0;j<n-i-1;j++){
+      if(arr[j]<arr[j+1]){
+        int temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        swapped = true;
+      }
     }
+    if(swapped == false) break;
+  }
 }
 
-void bubbleSortDescending(int arr[], int n){
-    int i, j;
-    bool swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] < arr[j + 1]) {
-                swap(&arr[j], &arr[j + 1]);
-                swapped = true;
-            }
-        }
 
-        
-        if (swapped == false)
-            break;
+int main(){
+  int p;
+  int n;
+  printf("Enter size of Array: ");
+  scanf("%d",&n);
+  
+  int arr[n];
+  printf("Enter Array elements: ");
+  for(int i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+  
+  printf("Hello My PID %d Parent pid is :%d, \n",getpid(),getppid());
+  
+  sleep(2);
+  
+  p = fork();
+  
+  if(p<0){
+    printf("\nError");
+    exit(1);
+  }else if(p==0){
+    bubbleSortAscending(arr, n);
+    for(int i=0;i<n;i++){
+      printf("i = %d arr[i] = %d pid = %d ppid = %d\n",i,arr[i],getpid(),getppid());
+      
     }
-}
-
-int main()
-{
-    int p,i=0;
-    int n;
-    printf("Enter size of array: ");
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++)
-     scanf("%d",&arr[i]);
-    
-    printf("Hello My PID %d Parent pid is :%d, \n",getpid(),getppid());
-    sleep(2);
-    p=fork(); 
-    if(p<0){
-      printf("\n Error ");
-      exit(1);
-    }else if(p==0){
-        bubbleSortDescending(arr,n);
-        for(int i=0;i<n;i++){
-            printf("i= %d  arr[i]= %d p = %d ppid = %d \n",i,arr[i],getpid(),getppid());
-            sleep(1); // Zombie
-            // sleep(10);  // orphan
-        }
-        printf("\n");
+    printf("\n");
         printf("Child procees terminates\n");
-    }else{
-        bubbleSortAscending(arr, n);
-        for(int i=0;i<n;i++){
-            printf("i= %d  arr[i]= %d p = %d ppid = %d \n",i,arr[i],getpid(),getppid());
-            sleep(10); // Zombie
-          // sleep(1); // orphan
-        }
-       // wait();
-        printf("\n");
-        printf("Parent procees terminates\n");
-
+  }else{
+    bubbleSortDescending(arr, n);
+    for(int i=0;i<n;i++){
+      printf("i = %d arr[i] = %d pid = %d ppid = %d \n",i,arr[i],getpid(),getppid());
+      
     }
-  printf("PID %d Parent pid is :%d, \n",getpid(),getppid());
-return 0;    
+    printf("\n");
+        printf("Parent procees terminates\n");
+  }
+  
+  printf("Hello My PID %d Parent pid is :%d, \n",getpid(),getppid());
+  
+  return 0;
 }
